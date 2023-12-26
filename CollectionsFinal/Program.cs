51099f56af9878e19a01816b6e1ae7645ec70c32
Stanislav <slava.telyatnik@mail.ru>
2023-12-26 19:17:13 +0100

    Добавьте файлы проекта.

---
+++ CollectionsFinal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string folderPath = @"C:\Users\Stanislav\Desktop";
+
+        List<string> listCollection = new List<string>();
+        LinkedList<string> linkedListCollection = new LinkedList<string>();
+
+        string[] files = Directory.GetFiles(folderPath, "*.txt");
+
+        Stopwatch listStopwatch = new Stopwatch();
+        listStopwatch.Start();
+
+        foreach (string file in files)
+        {
+            string content = File.ReadAllText(file);
+            listCollection.Add(content);
+        }
+
+        listStopwatch.Stop();
+        Console.WriteLine($"Вставка в List<T> заняла {listStopwatch.Elapsed}");
+
+        listCollection.Clear();
+
+        Stopwatch linkedListStopwatch = new Stopwatch();
+        linkedListStopwatch.Start();
+
+        foreach (string file in files)
+        {
+            string content = File.ReadAllText(file);
+            linkedListCollection.AddLast(content);
+        }
+
+        linkedListStopwatch.Stop();
+        Console.WriteLine($"Вставка в LinkedList<T> заняла {linkedListStopwatch.Elapsed}");
+        Console.ReadLine();
+    }
+}
